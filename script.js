a541bcb284dbffd35857bfe3f5aac133f311d3e3
Федор <fedor531@yandex.ru>
2021-01-25 18:12:35 +0700

    refactor

--- script.js
+++ script.js
@@ -2,34 +2,25 @@
 const banners = document.querySelectorAll('.banner');
 
 function setBannersInStorage() {
-  banners.forEach((b) => {
+  banners.forEach((node) => {
     if (
       window.innerHeight + window.scrollY >=
-        b.getBoundingClientRect().bottom + window.scrollY &&
-      b.getBoundingClientRect().top + window.scrollY >= window.scrollY &&
-      !localStorage.getItem(`${b.id}`)
+      node.getBoundingClientRect().bottom + window.scrollY &&
+      node.getBoundingClientRect().top + window.scrollY >= window.scrollY &&
+      !localStorage.getItem(`${node.id}`)
     ) {
-      localStorage.setItem(`${b.id}`, 'ok');
-      console.log(`Баннер с id - ${b.id} записан в localStorage`);
+      localStorage.setItem(`${node.id}`, 'ok');
+      console.log(`Баннер с id - ${node.id} записан в localStorage`);
     }
   });
-
-  // Когда все баннеры просмотрены - удаляем слушатель
-  if (
-    [...banners].every((b) => {
-      return Object.keys(localStorage).some((localBannerKey) => {
-        return localBannerKey === b.id;
-      });
-    })
-  ) {
-    window.removeEventListener('scroll', setBannersInStorage);
-  }
 }
 
 // Добавление слушателя
-window.addEventListener('scroll', setBannersInStorage);
+if (banners.length > 0) {
+  window.addEventListener('scroll', setBannersInStorage);
+}
 
 // Для тестов / Отчистка localStorage при перезагрузке страницы
-/* window.onbeforeunload = function () {
+window.onbeforeunload = function () {
   localStorage.clear();
-}; */
+};
